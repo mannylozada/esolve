7b6595ccb346a2fff546311f7dce811fdc326f6f
Manny Lozada <mannylozada@icloud.com>
2013-09-07 13:43:31 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Esolve::Application.config.secret_token = '44443455465a5bd78b2a1388d8dbf2af4b9d826e4e00da5adb0d43229327fdae257fdad6687cf86ee3151b5e9a332af0e08b6ecafa7dc1ab2b1fac9bd062d81f'
